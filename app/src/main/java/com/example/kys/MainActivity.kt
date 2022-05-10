ee321f6c3499d3acd3ddaf1aeb32db712fe1efac
Mert Yeşilbaş <yesilbas.mert@ogr.deu.edu.tr>
2022-05-10 17:13:42 +0300

    Bottom Nav Bar Actions Created

--- app/src/main/java/com/example/kys/MainActivity.kt
+++ app/src/main/java/com/example/kys/MainActivity.kt
@@ -2,12 +2,47 @@ package com.example.kys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.kys.fragments.CreateConfFragment
+import com.example.kys.fragments.HomeFragment
+import com.example.kys.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val createConfFragment = CreateConfFragment()
+        val profileFragment = ProfileFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when(item.itemId) {
+                R.id.home -> fragment = homeFragment
+                R.id.createConf -> fragment = createConfFragment
+                R.id.profile -> fragment = profileFragment
+            }
+            fragmentManager.beginTransaction().replace(R.id.mainFrame, fragment).commit()
+            true
+        }
+        bottomNavigationView.selectedItemId = R.id.home
     }
 
+
+
+//    private fun replaceFragment(fragment: Fragment) {
+//        if(fragment !=null){
+//            val transaction = supportFragmentManager.beginTransaction()
+//            transaction.replace(R.id.mainFrame, fragment)
+//            transaction.commit()
+//        }
+//    }
 }
