bb2ae1848a93d0d6041491fba8b3e570e1800e34
Mert Yeşilbaş <yesilbas.mert@ogr.deu.edu.tr>
2022-05-13 16:06:42 +0300

    Created Create Conference Date Section

--- app/src/main/java/com/example/kys/fragments/CreateConfFragment.kt
+++ app/src/main/java/com/example/kys/fragments/CreateConfFragment.kt
@@ -6,14 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kys.R
+import com.example.kys.databinding.FragmentCreateConfBinding
+import kotlinx.android.synthetic.main.fragment_create_conf.view.*
 
-class CreateConfFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_conf, container, false)
+class CreateConfFragment : Fragment(R.layout.fragment_create_conf) {
+    private var _binding: FragmentCreateConfBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentCreateConfBinding.bind(view)
+
+        binding.apply {
+            confDateTextInputLayout.setEndIconOnClickListener {
+                val datePickerFragment = DatePickerFragment()
+                val supportFragmentManager = requireActivity().supportFragmentManager
+
+                supportFragmentManager.setFragmentResultListener(
+                    "REQUEST_KEY",
+                    viewLifecycleOwner
+                ) { resultKey, bundle ->
+                    if (resultKey == "REQUEST_KEY") {
+                        val date = bundle.getString("SELECTED_DATE")
+                        confDateTextInputLayout.confDateEditText.setText(date)
+                    }
+                }
+
+                datePickerFragment.show(supportFragmentManager, "DatePickerFragment")
+            }
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
