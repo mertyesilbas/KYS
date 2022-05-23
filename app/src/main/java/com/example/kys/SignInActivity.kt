fb79ccf971682cc0e4ba18f99d051d409166bee3
Mert Yeşilbaş <yesilbas.mert@ogr.deu.edu.tr>
2022-05-23 21:25:31 +0300

    Created Logout Button on Profile Fragment

--- app/src/main/java/com/example/kys/SignInActivity.kt
+++ app/src/main/java/com/example/kys/SignInActivity.kt
@@ -20,33 +20,35 @@ class SignInActivity : AppCompatActivity() {
         val intent = Intent(this, SignUpActivity::class.java)
 
         firebaseAuth = FirebaseAuth.getInstance()
-        binding.textView.setOnClickListener{
+        binding.textView.setOnClickListener {
             startActivity(intent)
         }
 
-        binding.loginButton.setOnClickListener{
+        binding.loginButton.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty()) {
 
-                firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener{
-                    if (it.isSuccessful){
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
-                    }else{
+                    } else {
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                     }
                 }
-            }else{
-                Toast.makeText(this, "Bütün alanları doldurunuz!",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Bütün alanları doldurunuz!", Toast.LENGTH_SHORT).show()
             }
         }
+
     }
 
     override fun onStart() {
         super.onStart()
-
-        if (firebaseAuth.currentUser != null) {
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/kys/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/kys/fragments/ProfileFragment.kt
@@ -1,30 +1,34 @@
 package com.example.kys.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.kys.R
+import com.example.kys.SignInActivity
+import com.example.kys.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentProfileBinding.bind(view)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val intent = Intent(activity, SignInActivity::class.java)
+
+        binding.apply {
+            logoutButton.setOnClickListener{
+                FirebaseAuth.getInstance().signOut()
+                startActivity(intent)
+            }
+        }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
